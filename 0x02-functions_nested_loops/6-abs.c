39cb8bfe546d8e494dacb68e0072f169f67688f6
momo <gmohab37@yahoo.com>
2022-09-13 19:27:02 -0700

    task5-11

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -11,19 +11,19 @@
 int _abs(int n)
 {
 
-  if (n < 0)
-    {
-      return (n * (-1));
-    }
+if (n < 0)
+{
+return (n * (-1));
+}
 
-  else if (n == 0)
-    {
-      return (0);
-    }
+else if (n == 0)
+{
+return (0);
+}
 
-  else
-    {
-      return (n);
-    }
+else
+{
+return (n);
+}
 
 }
