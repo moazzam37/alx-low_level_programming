62bf81d63d602a4959eb30abc59076ac0ca721e5
momo <gmohab37@yahoo.com>
2022-09-15 18:41:30 -0700

    task0-4

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+nclude "main.h"
+/**
+ *  * _isupper - evalue if letter is uppercase .
+ *   *@c: print int
+ *    * Return: Always 0.
+ *     */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
+
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+nclude "main.h"
+/**
+ *  * mul -multiplies two integers
+ *   *@a: print int
+ *    *@b: print int
+ *     * Return: Always 0.
+ *      */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
