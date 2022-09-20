b318fc254d566a3f2d3f3bd6a5df2d2d0cec1117
momo <gmohab37@yahoo.com>
2022-09-19 18:40:18 -0700

    task4-9

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 /**
  *  * rev_string - prints a reverse string
  *   *@s: A pointer to an int that will be changed
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  *  *puts2 - prints every character of a string
