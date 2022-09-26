bf08b3f4ea148d5e8570ce0c5797c9ccafd294be
momo <gmohab37@yahoo.com>
2022-09-26 15:37:08 -0700

    task1-4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,7 @@
  *   * @s: This is the C string to be scanned.
  *    * @accept:character in str1 that matches one of the characters in str2
  *     *Return: string s that matches any character specified in accept
- **/
+ */
 
 char *_strpbrk(char *s, char *accept)
 {
