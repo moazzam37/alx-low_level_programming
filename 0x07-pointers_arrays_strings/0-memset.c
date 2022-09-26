958c45fb2e3587f5e650cbac2b40d2f3da623db7
momo <gmohab37@yahoo.com>
2022-09-26 15:17:19 -0700

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,20 +5,20 @@
  *    *@b: value to set
  *     *@n: bytes of the memory
  *      *Return: dest
- *       */
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 
 {
-		unsigned int i;
+unsigned int i;
 
 			/*Delacring FOR*/
-			for (i = 0; i < n; i++)
-					{
-								*(s + i) = b; /*add 1 position s*/
+for (i = 0; i < n; i++)
+{
+*(s + i) = b; /*add 1 position s*/
 
-									} /*END FOR*/
+} /*END FOR*/
 
-				return (s);
+return (s);
 }
 
